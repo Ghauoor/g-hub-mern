7de0aa0a09ba6691f30dd1cc8414ee2b95773252
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-11-25 16:03:06 +0500

    Get the Response from github URL Completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,53 @@
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Spinner from "../components/Spinner";
+import { useEffect } from "react";
+
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("fork");
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(`https://api.github.com/users/Ghauoor`);
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+      const reposRes = await fetch(userProfile.repos_url);
+      const repos = await reposRes.json();
+      setRepos(repos);
+      setLoading(false);
+
+      console.log(
+        "User profile and repos fetched successfully",
+        userProfile,
+        repos
+      );
+    } catch (error) {
+      toast.error(error.message || "Error fetching user profile and repos");
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
   return (
     <div className="m-4">
-      {/* <Search />
-      <SortRepos /> */}
+      <Search />
+      <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        {/* <ProfileInfo />
-        <Repos />
-        <Spinner /> */}
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {repos && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
